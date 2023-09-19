35b337339a93fde16a7d4733a8ae15a99165eb92
PecaLee <studiopeca.lee@gmail.com>
2023-09-19 22:23:21 +0900

    230919

--- 09_flask/app.py
+++ 09_flask/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask("minimalServer")
 
 
 @app.route("/")
-def hello_world():
-    return "hello world"
+def home():
+    args = request.args
+    return render_template("home.html", args=args)
