2a453fb3b0233fac50d92522b5c15d3dd0825dc2
PecaLee <studiopeca.lee@gmail.com>
2023-09-23 20:42:56 +0900

    230923

--- 09_flask/app.py
+++ 09_flask/app.py
@@ -5,5 +5,5 @@ app = Flask("minimalServer")
 
 @app.route("/")
 def home():
-    listing_num = [1,2,3,4,5,6,7,8,9,10]
+    listing_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     return render_template("home.html", args=listing_num)
